7b567e205a82e302613e647fd5dc559582a29251
Lenovo <Lenovo@Lenovo-PC>
2021-08-11 16:36:36 +0530

    Its a First Commit...

---
+++ src/GitProject_1/SampleClass.java
@@ -0,0 +1,31 @@
+package GitProject_1;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+	What_Is_Git();
+	FeaturesOfGit();
+	}
+	static void What_Is_Git() 
+	{	System.out.println("What is Git:-");
+		System.out.println("Git is a version control system used for tracking changes in computer files. It is generally used for source code management in software development.\r\n"
+				+ "Git is used to tracking changes in the source code\r\n"
+				+ "The distributed version control tool is used for source code management\r\n"
+				+ "It allows multiple developers to work together\r\n"
+				+ "It supports non-linear development through its thousands of parallel branches");
+		System.out.println("-------------------------------------------------------------------");
+	}
+	static void FeaturesOfGit() 
+	{
+		System.out.println("Features Of Git:-");
+		System.out.println("Tracks history\r\n"
+				+ "Free and open source\r\n"
+				+ "Supports non-linear development\r\n"
+				+ "Creates backups\r\n"
+				+ "Scalable\r\n"
+				+ "Supports collaboration\r\n"
+				+ "Branching is easier\r\n"
+				+ "Distributed development");
+	}
+
+}
